db02ee1e0a05bf6f3e3f0b172049b3cf959869ca
NadhifaAraineyda <a710230006@student.ums.ac.id>
2024-06-09 19:18:38 +0700

    day3.py

---
+++ day3.py
@@ -0,0 +1,10 @@
+print("Menghitung Akar Pangkat 3")
+
+#mengambil inputan angka
+angka = int(input("masukkan angka : "))
+
+#menghitung akar pangkat 3
+hasil = angka **(1/3)
+
+#menampilkan hasil
+print("hasil akhir pangkat 3 : ", hasil)
